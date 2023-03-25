30d7853b2a585f91e69b30d29d57d0c620fdbaef
LucasDCoder <LucasDCoder@gmail.com>
2023-03-24 21:46:54 -0300

    Proyecto Final 2

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,92 +1,89 @@
+import { Router } from 'express';
+import CartManager from '../dao/fs/CartManager.js';
+import cartDao from '../dao/cartDao.js';
+import productDao from '../dao/productDao.js';
+import { cartModel } from '../dao/models/cart.models.js';
 
-import { Router } from 'express'
-import CartManager from '../dao/fs/CartManager.js'
-import cartDao from '../dao/cartDao.js'
-import productDao from '../dao/productDao.js'
-import { cartModel } from '../dao/models/cart.models.js'
-
-const router = Router()
+const router = Router();
 
 router.get('/', async (req, res) => {
   try {
-    res.json(await cartDao.getCarts());
+    const carts = await cartDao.getCarts();
+    res.json(carts);
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error: error.message });
   }
-})
-
+});
 
 router.get('/:cid', async (req, res) => {
-
-  const cid = (req.params.cid)
+  const cid = req.params.cid;
   try {
-    res.json(await cartDao.getCartById(cid))
+    const cart = await cartDao.getCartById(cid);
+    res.json(cart);
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
 router.post('/', async (req, res) => {
   try {
-    await cartDao.createCart(req.body)
-    res.json({ status: 'Success' })
+    await cartDao.createCart(req.body);
+    res.status(201).json({ message: 'Carrito creado exitosamente' });
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
 router.put('/:cid', async (req, res) => {
   const { cid } = req.params;
-  let enExistencia;
   const { quantity, pid } = req.body;
-  // Validando si tenemos un producto en nuestra lista con el id que queremos agregar al carrito
   try {
-    enExistencia = await productDao.getProductById(pid)
-    if (!enExistencia) res.status(404).json({ "error": "El producto no se encuentra en la base de datos" })
-    else {
-      try {
-        let product = { product: pid, quantity: quantity }
-        cartDao.updateCart(cid, product)
-        res.json({ message: 'Carrito Actualizado' })
-      } catch (error) {
-        res.json({ error })
+    const existingProduct = await cartDao.getCartProductById(cid, pid);
+    if (existingProduct) {
+      res.status(409).json({ error: 'El producto ya existe en el carrito' });
+    } else {
+      const product = await productDao.getProductById(pid);
+      if (!product) {
+        res.status(404).json({ error: 'El producto no se encuentra en la base de datos' });
+      } else {
+        await cartDao.updateCart(cid, { product: pid, quantity });
+        res.json({ message: 'Carrito actualizado exitosamente' });
       }
     }
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error: error.message });
   }
-})
+});
+
 router.put('/:cid/products/:pid', async (req, res) => {
   const { cid, pid } = req.params;
   const { quantity } = req.body;
   try {
-    await cartDao.updateQuantityToCart(cid, pid, quantity)
-    res.json({ message: 'Cantidades Actualizadas' })
+    await cartDao.updateQuantityToCart(cid, pid, quantity);
+    res.json({ message: 'Cantidades actualizadas exitosamente' });
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error: error.message });
   }
-})
-
+});
 
 router.delete('/:cid/products/:pid', async (req, res) => {
   const { cid, pid } = req.params;
   try {
-    await cartDao.deleteProductFromCart(cid, pid)
-    res.json({ status: 'success', info: 'Producto eliminado de carrito' })
+    await cartDao.deleteProductFromCart(cid, pid);
+    res.json({ message: 'Producto eliminado del carrito exitosamente' });
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error: error.message });
   }
+});
 
-})
-
-router.delete('/:cid', async (req,res) => {
-  const {cid} = req.params; 
+router.delete('/:cid', async (req, res) => {
+  const { cid } = req.params;
   try {
-   await cartDao.emptyCart(cid) 
-    res.json({message:'Carrito Vaciado'})
+    await cartDao.emptyCart(cid);
+    res.json({ message: 'Carrito vaciado exitosamente' });
   } catch (error) {
-    res.json({error})
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
-export default router; 
+export default router;
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,79 +3,83 @@ import { uploader } from '../utils/multer.js'
 import productDao from '../dao/productDao.js'
 import { productModel } from '../dao/models/products.models.js'
 
-
-
-
 const router = Router()
 
-
 router.get('/', async (req, res) => {
-
-  let limit = parseInt(req.query.limit)
-  // let query = JSON.parse(req.query.query) 
-  let query = req.query.query || null
-  let sort = parseInt(req.query.sort)
-  let page = parseInt(req.query.page)
+  const limit = parseInt(req.query.limit)
+  const query = req.query.query || null
+  const sort = parseInt(req.query.sort)
+  const page = parseInt(req.query.page)
 
   try {
-    let result = await productDao.getProducts(limit, JSON.parse(query), sort, page)
-    res.json(
-      {
-        status: 'success',
-        payload: result,
-      })
+    const result = await productDao.getProducts(limit, JSON.parse(query), sort, page)
+    res.json({
+      status: 'success',
+      payload: result,
+    })
   } catch (error) {
-    res.json({ message: 'Ha ocurrido un error, verifique bien los datos ingresados' })
+    res.status(500).json({ error })
   }
 })
-
 
 router.get('/:pid', async (req, res) => {
-  let pid = (req.params.pid);
+  const pid = req.params.pid
+
   try {
-    res.json(await productDao.getProductById(pid))
+    const product = await productDao.getProductById(pid)
+    if (!product) {
+      res.status(404).json({ message: `Product with ID ${pid} not found` })
+    } else {
+      res.json(product)
+    }
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error })
   }
 })
 
-
-
 router.post('/', uploader.single('thumbnail'), async (req, res) => {
-  const { title, description, category, price, thumbnail, code, stock } = req.body;
-  !req.file && res.status(400).send({ status: "error", error: "No se pudo guardar la imagen" })
-  let thumbnailName = req.file.filename || 'Sin Imagen';
+  const { title, description, category, price, code, stock } = req.body
+  const thumbnailName = req.file?.filename || 'Sin Imagen'
+
   try {
-    let addedProduct = await productDao.createProduct({
-      title, description, category, price, thumbnailName, code, stock
+    const addedProduct = await productDao.createProduct({
+      title,
+      description,
+      category,
+      price,
+      thumbnailName,
+      code,
+      stock,
     })
-    res.status(201).json({ info: 'Producto Agregado', addedProduct })
+
+    res.status(201).json({ info: 'Product added', addedProduct })
   } catch (error) {
-    console.log("Ha ocurrido un error: \n", error)
-    res.status(400).json({ info: `Ha ocurrido un error: ${error}` })
+    console.error(error)
+    res.status(500).json({ error })
   }
 })
 
 router.put('/:pid', async (req, res) => {
-  const pid = (req.params.pid)
+  const pid = req.params.pid
   const updatedValue = req.body
+
   try {
     await productDao.updateProduct(pid, updatedValue)
-    res.send({ status: 200, payload: updatedValue })
+    res.status(200).json({ status: 'success', payload: updatedValue })
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error })
   }
 })
 
 router.delete('/:pid', async (req, res) => {
-  let pid = (req.params.pid)
+  const pid = req.params.pid
+
   try {
     await productDao.deleteProduct(pid)
-    res.json({ status: 200, message: 'Producto producto eliminado' })
+    res.status(200).json({ status: 'success', message: 'Product deleted' })
   } catch (error) {
-    res.json({ error })
+    res.status(500).json({ error })
   }
 })
 
-export default router;
-
+export default router
